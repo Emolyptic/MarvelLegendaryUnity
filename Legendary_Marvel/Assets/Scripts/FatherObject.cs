2efffcfd6cef33ba0cc1ed659ed5227be6cf4fbf
emolyptic <emolyptic@gmail.com>
2015-07-14 20:30:25 -0600

    More Pushing Base

---
+++ Legendary_Marvel/Assets/Scripts/FatherObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FatherObject : MonoBehaviour {
+	
+	Card mastermind;
+	Card scheme;
+
+	List<string> cardLibrary;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
