f5792d0f5ac37c6bec2e1d21147b39a94394e7b9
emolyptic <emolyptic@gmail.com>
2015-07-14 22:43:21 -0600

    this

--- Legendary_Marvel/Assets/Scripts/MainGame.cs
+++ Legendary_Marvel/Assets/Scripts/MainGame.cs
@@ -2,14 +2,86 @@
 using System.Collections;
 
 public class MainGame : MonoBehaviour {
+	int phase;
+
+	bool chooseplayer		= false;
+	bool chooseheroes		= false;
+	bool choosehenchmen		= false;
+	bool choosevillain		= false;
+	bool choosemastermind	= false;
+	bool choosescheme 		= false;
 
 	// Use this for initialization
 	void Start () {
-	
+		phase = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(phase == 0) //Setup
+		{	chooseplayer = true;
+			SetupUpdate();
+		}
+		else if(phase == 1)
+		{
+
+		}
+	}
+	void SetupUpdate()
+	{
+
+	}
+	void OnGUI()
+	{
+		if(phase = 0)
+		{
+			if(chooseplayer)
+			{
+				if(GUI.Button(new Rect(0,0,50,50),"Next"))
+				{
+					chooseheroes = true;
+					choosplayer = false;
+				}
+			}
+			if(chooseheroes)
+			{
+				if(GUI.Button(new Rect(0,0,50,50),"Next"))
+				{
+					choosehenchmen = true;
+					chooseheroes = false;
+				}
+			}
+			if(choosehenchmen)
+			{
+				if(GUI.Button(new Rect(0,0,50,50),"Next"))
+				{
+					choosevillain = true;
+					choosehenchmen = false;
+				}
+			}
+			if(choosevillain)
+			{
+				if(GUI.Button(new Rect(0,0,50,50),"Next"))
+				{
+					choosemastermind = true;
+					choosevillain = false;
+				}
+			}
+			if(choosemastermind)
+			{
+				if(GUI.Button(new Rect(0,0,50,50),"Next"))
+				{
+					choosescheme = true;
+					choosemastermind = false;
+				}
+			}
+			if(choosescheme)
+			{
+				if(GUI.Button(new Rect(0,0,50,50)," Next"))
+				{
+					choosescheme = false;
+				}
+			}
+		}
 	}
 }
